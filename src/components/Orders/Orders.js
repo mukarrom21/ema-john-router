9d8c57eb7bbcbbf0fe2722bf93443a73ed885b88
Mukarrom <mukarrom21@yahoo.com>
2022-04-01 22:35:44 +0600

    orders page cart added

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
 
 const Orders = () => {
-    return (
-        <div>
-            <h2>This is orders components</h2>
-        </div>
-    );
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
